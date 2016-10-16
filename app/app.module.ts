f5840a99c858a0ab332b7713371da1e907fb0811
Loïc Goyet <loic@appventus.com>
2016-10-16 09:15:27 +0200

    start to use http instead of mock

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule }    from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -9,13 +15,14 @@ import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroService }         from './hero.service';
 
-import { AppRoutingModule }    from './app-routing.module';
 
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule,
     ],
     declarations: [
@@ -24,10 +31,7 @@ import { AppRoutingModule }    from './app-routing.module';
         HeroesComponent,
         DashboardComponent,
     ],
-    providers: [
-        HeroService
-    ],
+    providers: [ HeroService ],
     bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
