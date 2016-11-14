dcb7f28e446230f5009ae8ebaf9cf65f97b01fbf
Loïc Goyet <loic@appventus.com>
2016-11-14 14:55:56 +0100

    implement angular quickstart architecture

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-        </nav>
-        <router-outlet></router-outlet>
-    `,
-    styleUrls: ['app.component.css'],
+    template: `<h1>Hello Angular</h1>`
 })
-export class AppComponent {
-    title = 'Tour of Heroes';
-}
+export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,37 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
 
-import { AppRoutingModule }    from './app-routing.module';
-
-// Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
-import { InMemoryDataService }  from './in-memory-data.service';
-
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroService }         from './hero.service';
-
-
+import { AppComponent }  from './app.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        InMemoryWebApiModule.forRoot(InMemoryDataService),
-        AppRoutingModule,
-    ],
-    declarations: [
-        AppComponent,
-        HeroDetailComponent,
-        HeroesComponent,
-        DashboardComponent,
-    ],
-    providers: [ HeroService ],
-    bootstrap: [ AppComponent ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
